bedc9755b716890600cb0b3384d4215c66aa0fbb
jingqiwei <jingqiwei@clx.cn>
2023-07-12 18:03:46 +0800

    新增打包脚本

---
+++ script/deploy.sh
@@ -0,0 +1,28 @@
+# /bin/bash
+#用于打包静态网站，并部署到 Github
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 重新打包组件库
+
+npm build
+
+# 打包生成静态文件
+npm docs:build
+
+# 进入待发布的 dist/ 目录
+cd dist
+
+git init
+git add .
+git commit -m 'deploy'
+
+# 部署到 https://.github.io/
+git push -f https://github.com/jqw755/q-drag-m.git master:gh-pages
+
+# 提交所有代码到github
+cd ../
+git add .
+git commit -m 'update'
+git push
